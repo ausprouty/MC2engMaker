44639fc9b8a51dbfdee2eb958d4c5b7df3b7119a
ausprouty <bob.prouty@powertochange.org.au>
2023-04-15 08:38:01 +1000

    first commit

---
+++ src/views/returnToLastPage.vue
@@ -0,0 +1,20 @@
+<template>
+ </template>
+ <script>
+ export default{
+    beforeCreate() {
+        
+    var returnTo = 'eng-index'
+    if (localStorage.getItem("lastpage")){
+        returnTo = localStorage.getItem("lastpage")
+        localStorage.removeItem("lastpage")
+    }
+    this.$router.push({
+        name:returnTo,
+    })
+  },
+}
+ 
+ </script>
+ 
+  
